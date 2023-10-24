55dd9cfb8e3b9d95008273dbb1fc5b58807ce911
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-23 19:39:10 -0700

    seperated file

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-class Shape {
-    constructor(    )
-}
-`
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+const Shape = require('./lib/shapes')
 
-  <circle cx="150" cy="100" r="80" fill="green"/>
-
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-
-</svg>
-`
